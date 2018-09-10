ff0222d892dfa6ff1935ff1df0d35e79a88f1b1f
mingxiao <mingxiao.huang@intel.com>
2018-09-10 15:20:26 +0800

    add augmentation test script

--- train_imagenet_resnet50_blockmode_augmentation.py
+++ train_imagenet_resnet50_blockmode_augmentation.py
@@ -17,23 +17,33 @@ import numpy as np
 import chainer
 from chainer import training
 from chainer.training import extensions
+import cv2
+from PIL import Image
+import glob
+import random
+import skimage
+import skimage.io as io
+import skimage.transform
+import sys
+import numpy as np
+import math
+#from pylab import *
+#from matplotlib import pyplot
+#import matplotlib.image as mpimg
 
-import alex
-import googlenet
 import resnet50
-import googlenetbn
-import nin
-import vgg16
 
 
 class PreprocessedDataset(chainer.dataset.DatasetMixin):
 
-    def __init__(self, path, root, mean, crop_size, random=True):
+    def __init__(self, path, root, mean, crop_size, min_aspect_ratio_change, min_area_change, max_area_change, random = True):
         self.base = chainer.datasets.LabeledImageDataset(path, root)
         self.mean = mean.astype('f')
         self.crop_size = crop_size
+        self.min_aspect_ratio_change = min_aspect_ratio_change
+        self.min_area_change =  min_area_change
+        self.max_area_change = max_area_change
         self.random = random
-
     def __len__(self):
         return len(self.base)
 
@@ -46,37 +56,95 @@ class PreprocessedDataset(chainer.dataset.DatasetMixin):
         crop_size = self.crop_size
 
         image, label = self.base[i]
-        _, h, w = image.shape
+        ch, h, w = image.shape
 
-        if self.random:
-            # Randomly crop a region and flip the image
-            top = random.randint(0, h - crop_size - 1)
-            left = random.randint(0, w - crop_size - 1)
-            if random.randint(0, 1):
-                image = image[:, :, ::-1]
-        else:
-            # Crop the center
-            top = (h - crop_size) // 2
-            left = (w - crop_size) // 2
+        top = (h - crop_size) // 2
+        left = (w - crop_size) // 2
         bottom = top + crop_size
         right = left + crop_size
-
-        image = image[:, top:bottom, left:right]
-        image -= self.mean[:, top:bottom, left:right]
+        #if ch != 3:
+        #   image = np.resize(image, (3, h, w))
+        #    print("before:", image.shape)
+        #    im = image#Image.open(image)
+        #    image = im.convert('RGB')
+        #    print("after:", image.shape)
+        img_mean = np.ones((3, h, w), dtype=np.float32)
+        for i in range(0, 3):
+           if i == 0:
+              mean_value = 104.0
+           elif i == 1:
+              mean_value = 117.0
+           elif i == 2:
+              mean_value = 123.0
+           else:
+              print("unexpected parameter")
+           img_mean[i] *= mean_value
+        #mean_values_R = np.ones((h, w), dtype=np.float32)* 104
+        #mean_values_G = np.ones((h, w), dtype=np.float32)* 117
+        #mean_values_B = np.ones((h, w), dtype=np.float32)* 123
+        image -=img_mean
+        #image -= self.mean
         image *= (1.0 / 255.0)  # Scale to [0, 1]
-        return image, label
 
+        if self.random:
+            #image shape change to H,W,C
+            image = image.swapaxes(1, 0).swapaxes(2, 1)
+            random_flip = random.random()
+            if random_flip >= 0.5:
+                imgMirror = np.fliplr(image)
+            else:
+                imgMirror = image
+            area = imgMirror.shape[0] * imgMirror.shape[1]
+            i=0
+            while i < 10:
+                i = i+1
+                aspect_ratio_change = random.uniform(self.min_aspect_ratio_change, 1/self.min_aspect_ratio_change)
+                area_ratio = random.uniform(self.min_area_change, self.max_area_change)
+                new_area = area * area_ratio
+                new_height = int(math.sqrt(new_area) * aspect_ratio_change)
+                new_width = int(math.sqrt(new_area) / aspect_ratio_change)
+                if random.randint(0, 1):
+                #if new_height < new_width:
+                    new_height, new_width = new_width, new_height
+                if new_height <= imgMirror.shape[0] and new_width <= imgMirror.shape[1]:
+                #if crop_size <= new_height and  crop_size <= new_width:
+                #imgScaled = skimage.transform.resize(imgMirror, (new_height, new_width))
+                #start_y = random.randint(0, int(imgScaled.shape[0] - corp_size))
+                #start_x = random.randint(0, int(imgScaled.shape[1] - crop_szie))
+                    start_y = imgMirror.shape[0]- new_height + 1
+                    start_x = imgMirror.shape[1]- new_width + 1
+                    output_img = imgMirror[start_y : start_y + new_height, start_x : start_x + new_width]
+                    image = skimage.transform.resize(output_img, (crop_size, crop_size))
+                    image = image.swapaxes(1, 2).swapaxes(0, 1)
+                    #return image
+                else:
+                #new_aspect_ratio_change = (float(new_width) / float(new_height))
+                #new_height = int(float(crop_size)/ float(new_aspect_ratio_change))
+                    #     """when height is larger"""
+                #new_width = max(new_width, crop_size)
+                    image = skimage.transform.resize(imgMirror, (crop_size, crop_size))
+                #start_y = random.randint(0, max(0, int(imgScaled.shape[0] - crop_size)))
+                #output_img = imgScaled[start_y:start_y+crop_size, 0:crop_size]
+                    image = image.swapaxes(1, 2).swapaxes(0, 1)
+                 #print ("else:" + str(output_img.shape))
+                    #return image
+
+            # Randomly crop a region and flip the image
+            #top = random.randint(0, h - crop_size - 1)
+            #left = random.randint(0, w - crop_size - 1)
+            #if random.randint(0, 1):
+            #    image = image[:, :, ::-1]
+        else:
+            # Crop the center
+            image = image[:, top:bottom, left:right]
+        image = image.astype(np.float32)
+        #image -= self.mean[:, top:bottom, left:right]
+        #image *= (1.0 / 255.0)  # Scale to [0, 1]
+        return image, label
 
 def main():
     archs = {
-        'alex': alex.Alex,
-        'alex_fp16': alex.AlexFp16,
-        'googlenet': googlenet.GoogLeNet,
-        'googlenetbn': googlenetbn.GoogLeNetBN,
-        'googlenetbn_fp16': googlenetbn.GoogLeNetBNFp16,
-        'nin': nin.NIN,
         'resnet50': resnet50.ResNet50,
-        'vgg16': vgg16.VGG16
     }
 
     parser = argparse.ArgumentParser(
@@ -125,10 +193,20 @@ def main():
         model.to_intel64()
     # Load the datasets and mean file
     mean = np.load(args.mean)
+
+    min_aspect_ratio_change = 0.75
+    min_area_change = 0.08
+    max_area_change = 1.0
+    train = PreprocessedDataset(
+        args.train, args.train_root, mean, model.insize, min_aspect_ratio_change, min_area_change, max_area_change)
+    val = PreprocessedDataset(
+        args.val, args.val_root, mean, model.insize, min_aspect_ratio_change, min_area_change, max_area_change, False)
+    '''
     train = PreprocessedDataset(
         args.train, args.train_root, mean, model.insize)
     val = PreprocessedDataset(
         args.val, args.val_root, mean, model.insize, False)
+    '''
     # These iterators load the images with subprocesses running in parallel to
     # the training/validation.
     train_iter = chainer.iterators.MultiprocessIterator(
